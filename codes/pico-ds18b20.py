6a6b33869a5fb85eb3c4879b2b7babe8b73e9078
Tomaz <109036088+Tomaz-Arlindo@users.noreply.github.com>
2022-08-24 20:53:23 -0300

    Update pico-ds18b20.py

--- codes/pico-ds18b20.py
+++ codes/pico-ds18b20.py
@@ -1,14 +1,14 @@
+#importação das bibliotecas para pinos, protocolo, sensor e tempo 
 import machine, oneWire, ds18x20, time
 
-pinoTemp = machine.Pin(6)
-sensTemp = ds18x20.DS18X20(oneWire.OneWire(pinoTemp))
-idTemp = sensTemp.scan()
+pinoTemp = machine.Pin(6) #define o pino utilizado na montagem (GPIO6 - pino 9)
+sensTemp = ds18x20.DS18X20(oneWire.OneWire(pinoTemp)) #define protocolo
+idTemp = sensTemp.scan() #identifica endereço dos sensores conectados
 print("Sensores encontrados:",idTemp)
 
 while True:
-    sensTemp.convert_temp()
+    sensTemp.convert_temp() #realiza conversão de temperatura (min 750ms)
     time.sleep_ms(750)
     for i in idTemp:
-        print(sensTemp.read_temp(i), "oC")
+        print(sensTemp.read_temp(i), "oC") #realiza leitura de cada sensor
         time.sleep(1)
-    print("\n")
