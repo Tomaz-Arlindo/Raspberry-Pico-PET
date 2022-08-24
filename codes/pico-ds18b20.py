17b4b15184a2fe6d628bc971da6d3050afe493ef
Tomaz <109036088+Tomaz-Arlindo@users.noreply.github.com>
2022-08-24 16:17:38 -0300

    Update pico-ds18b20.py

--- codes/pico-ds18b20.py
+++ codes/pico-ds18b20.py
@@ -1,7 +1,7 @@
 import machine, oneWire, ds18x20, time
 
 pinoTemp = machine.Pin(6)
-sensTemp = ds18x20.DS18X20(onewire.OneWire(pinoTemp))
+sensTemp = ds18x20.DS18X20(oneWire.OneWire(pinoTemp))
 idTemp = sensTemp.scan()
 print("Sensores encontrados:",idTemp)
 
@@ -9,6 +9,6 @@ while True:
     sensTemp.convert_temp()
     time.sleep_ms(750)
     for i in idTemp:
-        print(sensTemp.read_temp(i), "oC", end = ";")
-        time.sleep(3)
+        print(sensTemp.read_temp(i), "oC")
+        time.sleep(1)
     print("\n")
