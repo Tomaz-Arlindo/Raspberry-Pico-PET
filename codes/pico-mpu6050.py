db02ce8e2151e840eee2e8916d51c55521c18a9c
Tomaz <109036088+Tomaz-Arlindo@users.noreply.github.com>
2022-08-24 23:15:45 -0300

    Update pico-mpu6050.py

--- codes/pico-mpu6050.py
+++ codes/pico-mpu6050.py
@@ -1,13 +1,14 @@
-import machine
-import time
-import mpu6050
+#importação das bibliotecas
+import machine, time, mpu6050
 
+#define pinos para i2c
 i2c = machine.I2C(0, scl= machine.Pin(21), sda= machine.Pin(20), freq=400000)
-mpu6050.mpu6050_init(i2c)
-    
+mpu6050.mpu6050_init(i2c) #inicia configurações do mpu6050 na porta i2c escolhida
+
+#loop de leitura de dados
 while True:
-    #print("Temperature:\t", mpu6050.mpu6050_get_temp(i2c), "°C")
-    print("Accelerometer:\t", mpu6050.mpu6050_get_accel(i2c), "g")
-    #print("Gyroscope:\t", mpu6050.mpu6050_get_gyro(i2c), "°/s")
+    print("Temperature:\t", mpu6050.mpu6050_get_temp(i2c), "°C") #temperatura
+    print("Accelerometer:\t", mpu6050.mpu6050_get_accel(i2c), "g") #aceleração 3 eixos
+    print("Gyroscope:\t", mpu6050.mpu6050_get_gyro(i2c), "°/s") #giroscópio 3 eixos
     time.sleep_ms(500)
     
